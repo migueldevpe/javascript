2c39198434218811ea45b6a69a12ddc6424040c8
Miguel Soares <miguelsoaresabs7@gmail.com>
2025-06-17 17:17:04 -0300

    up

---
+++ javascript.js
@@ -0,0 +1,19 @@
+/*var velocidade = 65;
+
+console.log(`Você está a ${velocidade}km/h.`);
+
+if (velocidade >= 60) {
+    console.log('Dirija com cuidado! VOCÊ SERÁ AUTUADO.');
+} else {
+    console.log('Boa! Você está dirigindo com cuidado.');
+}*/
+
+var país = 'BRASIL';
+
+console.log(`Vivendo em ${país}`);
+
+if (país == 'BRASIL') {
+    console.log('Você é Brasileiro.');
+} else {
+    console.log('Você é Estrangeiro.')
+}
